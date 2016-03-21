47fcec9fb5475ce127d951a12981720a02af5245
Aaron O'Brien <ayohbie@gmail.com>
2016-03-20 21:33:32 -0400

    Delete dbh.php

--- dbh.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-	$conn = mysqli_connect("mysql7.000webhost.com", "a7552541_admin", "password123", "a7552541_social");
-
-	if(!$conn){
-		die("Conneciton failed: ".mysqli_connect_error());
-	}
-?>
